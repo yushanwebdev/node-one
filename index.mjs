f3e8f6d5b7d57dd8fc497ef846d5003b1b6ca95b
yushanwebdev <yushanwebdev@gmail.com>
2023-04-15 20:46:20 +0530

    feat: Basic Components - Catch All

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import { readFile } from "fs/promises";
 
-try {
-  const result = await readFile(new URL("app.mj", import.meta.url), "utf-8");
-} catch (error) {
-  console.log("error", error);
-}
+process.on("uncaughtException", (e) => {
+  console.log("error", e);
+});
+
+const result = await readFile(new URL("app.mj", import.meta.url), "utf-8");
 
 console.log("hello");
