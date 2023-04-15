5c4ede0d96dfb62b3acce1329142212b59014d15
yushanwebdev <yushanwebdev@gmail.com>
2023-04-15 20:25:50 +0530

    feat: Basic Components - Error Handling using Async Callbacks

--- index.mjs
+++ index.mjs
@@ -1,17 +1,8 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile } from "fs";
 
-let template = await readFile(
-  new URL("template.html", import.meta.url),
-  "utf-8"
-);
-
-const data = {
-  title: "Learn Node.js",
-  body: "This is the final HTML",
-};
-
-for (const [k, v] of Object.entries(data)) {
-  template = template.replace(`{${k}}`, v);
-}
-
-await writeFile(new URL("index.html", import.meta.url), template);
+readFile(new URL("app.mj", import.meta.url), "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+  }
+});
