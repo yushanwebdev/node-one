8ae6393c19140285ff6d5c7f10e4092c04cc4f9f
yushanwebdev <yushanwebdev@gmail.com>
2023-04-22 13:34:57 +0530

    feat: Debugging

--- intro/index.mjs
+++ intro/index.mjs
@@ -4,6 +4,6 @@ process.on("uncaughtException", (e) => {
   console.log("error", e);
 });
 
-const result = await readFile(new URL("app.mj", import.meta.url), "utf-8");
+const result = await readFile(new URL("app.mjs", import.meta.url), "utf-8");
 
 console.log("hello");
